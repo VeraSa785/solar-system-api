00d64f70bdbac5863be41f7a18e3236eef90e1b7
Joanna <joannaparis7@gmail.com>
2022-04-26 12:05:00 -0700

    April 26 Wave 2

--- app/routes.py
+++ app/routes.py
@@ -20,6 +20,27 @@ planets = [
 
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
 
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        # return {"msg": f"Invalid planet ID: {planet_id}"}
+        rsp = {"msg": f"Invalid planet ID: {planet_id}"}
+        # return jsonify(rsp), 400
+    
+    chosen_planet = None
+    for planet in planets:
+        if planet.id == planet_id:
+            chosen_planet = planet
+            break
+    if chosen_planet is None:
+        # return {"msg": f"Could not find planet with ID: {planet_id}"}
+        
+        rsp = {"msg": f"Could not find planet with ID: {planet_id}"}
+        # return jsonify(rsp), 404
+    
+    
+
 @planets_bp.route("", methods=["GET"])
 def get_all_planets():
     planet_response = []
@@ -30,4 +51,21 @@ def get_all_planets():
             "distance_from_sun_million_mi": planet.distance_from_sun_million_mi,
             "length_of_year_earth_days": planet.length_of_year_earth_days
         })
-    return jsonify(planet_response)
+    return jsonify(planet_response), 200
+
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def get_one_planet(planet_id):
+    # planet = validate_planet(planet_id)
+    rsp = validate_planet(planet_id)
+    for planet in planets:
+        if planet.id == planet.id:
+            chosen_planet = planet
+        
+    rsp = {
+        "id": chosen_planet.id,
+        "name": chosen_planet.name,
+        "distance_from_sun_million_mi": chosen_planet.distance_from_sun_million_mi,
+        "length_of_year_earth_days": chosen_planet.length_of_year_earth_days
+    }
+    return jsonify(rsp), 200
+    
