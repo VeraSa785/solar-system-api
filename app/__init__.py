e589d29978f36b3e9463043b1fcc2c447884a260
Joanna <joannaparis7@gmail.com>
2022-05-04 12:17:57 -0700

    hope

--- app/__init__.py
+++ app/__init__.py
@@ -3,27 +3,29 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
-
-
 db = SQLAlchemy()
 migrate = Migrate()
 load_dotenv()
-
 # some changes
 def create_app(testing=False):
     app = Flask(__name__)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #deal with database
-    
     if testing == {"testing":True}:
-        
+        app.config['TEST_SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres@localhost:5432/planets_test_db"
+    else:
         app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres@localhost:5432/planets_development"
-    TEST_SQLALCHEMY_DATABASE_URI = postgresql+psycopg2://postgres@localhost:5432/planets_test_db
     from app.models.planets import Planet
-
     db.init_app(app) #initials sqalchemy object the database
     migrate.init_app(app, db)
-
     from .routes import planets_bp
     app.register_blueprint(planets_bp)
-
     return app
+
+
+
+
+
+
+
+
+
