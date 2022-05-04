e4681b0733b7f805af802223af01640a92403120
Joanna <joannaparis7@gmail.com>
2022-05-04 12:09:21 -0700

    bad

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,23 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+
 
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()
 
-def create_app():
+def create_app(testing=False):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #deal with database
-    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres@localhost:5432/planets_development"
-
+    
+    if testing == {"testing":True}:
+        
+        app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+psycopg2://postgres@localhost:5432/planets_development"
+    TEST_SQLALCHEMY_DATABASE_URI = postgresql+psycopg2://postgres@localhost:5432/planets_test_db
     from app.models.planets import Planet
 
     db.init_app(app) #initials sqalchemy object the database
