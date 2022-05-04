ec8023109e61afd7f07ad707118b8d05bace03f4
verasazonova <vera.zhokhova@gmail.com>
2022-05-04 11:57:47 -0700

    Wave 5 completed

--- app/routes.py
+++ app/routes.py
@@ -3,24 +3,7 @@ from requests import session
 from app.models.planets import Planet
 from app import db
 
-# class Planet:
-#     def __init__(self, id, name, distance_from_sun_million_mi, length_of_year_earth_days):
-#         self.id = id
-#         self.name = name
-#         self.distance_from_sun_million_mi = distance_from_sun_million_mi
-#         self.length_of_year_earth_days = length_of_year_earth_days
-
-# planets = [
-#     Planet(1, "Mercury", 35.98, 88),
-#     Planet(2, "Venus", 67.24, 225),
-#     Planet(3, "Earth", 92.96, 365.24),
-#     Planet(4, "Mars", 141.6, 693.96),
-#     Planet(5, "Jupiter", 462.31, 4346.36),
-#     Planet(6, "Saturn", 918.35, 10774.58),
-#     Planet(7, "Uranus", 1.8313, 30680.16),
-#     Planet(8, "Neptune", 2.7803, 60191.55)
-# ]
-
+# helper functions
 def get_planet_or_abort(planet_id):
     try:
         planet_id = int(planet_id)
@@ -31,8 +14,9 @@ def get_planet_or_abort(planet_id):
         abort(make_response({"msg": f"Could not find planet with ID: {planet_id}"}, 404))
     return planet
     
-
+# routes
 planets_bp = Blueprint("planets_bp", __name__, url_prefix="/planets")
+
 
 @planets_bp.route("", methods=["POST"])
 def create_one_planet():
@@ -47,9 +31,25 @@ def create_one_planet():
         "msg": f"Succesfully creates Planet with id {new_planet}"
         },201
 
+
 @planets_bp.route("", methods=["GET"])
 def get_all_planets():
-    planets = Planet.query.all()
+    params = request.args
+    if "name" in params and "distance_from_sun_million_mi" in params:
+        planet_name = params["name"]
+        distance_from_sun = params["distance_from_sun_million_mi"]
+        planets = Planet.query.filter_by(name = planet_name, distance_from_sun_million_mi = distance_from_sun)
+    elif "name" in params:
+        planet_name = params["name"]
+        planets = Planet.query.filter_by(name = planet_name)
+    elif "distance_from_sun_million_mi" in params:
+        distance_from_sun = params["distance_from_sun_million_mi"]
+        planets = Planet.query.filter_by(distance_from_sun_million_mi = distance_from_sun)
+    elif "length_of_year_earth_days" in params:
+        length_of_year = params["length_of_year_earth_days"]
+        planets = Planet.query.filter_by(length_of_year_earth_days = length_of_year)
+    else:
+        planets = Planet.query.all()
     planets_response = []
     for planet in planets:
         planets_response.append({
@@ -60,21 +60,6 @@ def get_all_planets():
         })
     return jsonify(planets_response), 300
 
-
-# get_planet_or_abort(planet_id)
-
-
-# @planets_bp.route("", methods=["GET"])
-# def get_all_planets():
-#     planet_response = []
-#     for planet in planets:
-#         planet_response.append({
-#             "id": planet.id,
-#             "name": planet.name,
-#             "distance_from_sun_million_mi": planet.distance_from_sun_million_mi,
-#             "length_of_year_earth_days": planet.length_of_year_earth_days
-#         })
-#     return jsonify(planet_response), 200
 
 @planets_bp.route("/<planet_id>", methods=["GET"])
 def get_one_planet(planet_id):
@@ -88,6 +73,7 @@ def get_one_planet(planet_id):
     }
     return jsonify(rsp), 200
 
+
 @planets_bp.route("/<planet_id>", methods=["PUT"])
 def put_one_planet(planet_id):
     planet = get_planet_or_abort(planet_id)
@@ -100,11 +86,11 @@ def put_one_planet(planet_id):
         return {
             "msg" : "name, distance from the sun, and length of year are required"
         }, 400
-        
     db.session.commit()
     return {
             "msg" : f"planet #{planet_id} successfully replaced"
         }, 200
+    
     
 @planets_bp.route("/<planet_id>", methods=["DELETE"])
 def delete_one_planet(planet_id):
